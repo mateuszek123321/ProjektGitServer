01b5d616acf36293dc1ca7b2c3ba05bb98ee07e6
mateuszek123321 <klosmateusz534@gmail.com>
2025-12-14 17:24:39 +0100

    "Initial commit"

---
+++ src/main/java/org/example/projektgitserverapp/ProjektGitServerAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.projektgitserverapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektGitServerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektGitServerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/projektgitserverapp/ProjektGitServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projektgitserverapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektGitServerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
