fca026e9dee6806ebf3c4ce044a5249bf686a2fe
Yauheni Ivus <128481048+zZo0mMeE@users.noreply.github.com>
2025-12-14 18:07:54 +0100

    added user repo

---
+++ src/main/java/org/example/projektgitserverapp/repositories/UserRepo.java
@@ -0,0 +1,14 @@
+package org.example.projektgitserverapp.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+    Optional<User> findByUserName(String userName);
+    Optional<User> findByUserEmail(String userEmail);
+
+}
